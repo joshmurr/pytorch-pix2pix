5881e27ae2eb7787e32849637cce1f3d94cbba37
Josh Murr <joshuadavidmurr@gmail.com>
2020-10-02 14:43:09 +0100

    Tweaks to make it work

--- network.py
+++ network.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class generator(nn.Module):
     # initializers
     def __init__(self, d=64):
@@ -56,11 +57,17 @@ class generator(nn.Module):
         e7 = self.conv7_bn(self.conv7(F.leaky_relu(e6, 0.2)))
         e8 = self.conv8(F.leaky_relu(e7, 0.2))
         # e8 = self.conv8_bn(self.conv8(F.leaky_relu(e7, 0.2)))
-        d1 = F.dropout(self.deconv1_bn(self.deconv1(F.relu(e8))), 0.5, training=True)
+        d1 = F.dropout(self.deconv1_bn(self.deconv1(F.relu(e8))),
+                       0.5,
+                       training=True)
         d1 = torch.cat([d1, e7], 1)
-        d2 = F.dropout(self.deconv2_bn(self.deconv2(F.relu(d1))), 0.5, training=True)
+        d2 = F.dropout(self.deconv2_bn(self.deconv2(F.relu(d1))),
+                       0.5,
+                       training=True)
         d2 = torch.cat([d2, e6], 1)
-        d3 = F.dropout(self.deconv3_bn(self.deconv3(F.relu(d2))), 0.5, training=True)
+        d3 = F.dropout(self.deconv3_bn(self.deconv3(F.relu(d2))),
+                       0.5,
+                       training=True)
         d3 = torch.cat([d3, e5], 1)
         d4 = self.deconv4_bn(self.deconv4(F.relu(d3)))
         # d4 = F.dropout(self.deconv4_bn(self.deconv4(F.relu(d3))), 0.5)
@@ -72,9 +79,10 @@ class generator(nn.Module):
         d7 = self.deconv7_bn(self.deconv7(F.relu(d6)))
         d7 = torch.cat([d7, e1], 1)
         d8 = self.deconv8(F.relu(d7))
-        o = F.tanh(d8)
+        o = torch.tanh(d8)
 
         return o
+
 
 class discriminator(nn.Module):
     # initializers
@@ -101,9 +109,10 @@ class discriminator(nn.Module):
         x = F.leaky_relu(self.conv2_bn(self.conv2(x)), 0.2)
         x = F.leaky_relu(self.conv3_bn(self.conv3(x)), 0.2)
         x = F.leaky_relu(self.conv4_bn(self.conv4(x)), 0.2)
-        x = F.sigmoid(self.conv5(x))
+        x = torch.sigmoid(self.conv5(x))
 
         return x
+
 
 def normal_init(m, mean, std):
     if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
